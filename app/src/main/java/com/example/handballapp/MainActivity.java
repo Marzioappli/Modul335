db0a74f1243a4e6ded1a8d0d449354a026922730
Marzio Cassese <marzio.cassese@edu.tbz.ch>
2024-01-17 23:08:50 +0100

    XML Quiz angefangen

--- app/src/main/java/com/example/handballapp/MainActivity.java
+++ app/src/main/java/com/example/handballapp/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity{
         quizbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Quiz.class);
+                Intent intent = new Intent(MainActivity.this, QuizActivity.class);
                 startActivity(intent);
             }
         });
